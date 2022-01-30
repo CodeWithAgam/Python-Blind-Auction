66c09cc36b11a6e6982c5f26cfe19c8a5f46922b
Agamdeep Singh <agamdeep579@gmail.com>
2022-01-30 19:14:07 +0530

    Added the file

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Python Blind Auction
- A blind auction built in Python
+ 
+A blind program auction built in Python
+
+## Example Run:
+```
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._    
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'     
+                          )"""""""(
+                         /_________\
+                       .-------------.
+                      /_______________\
+
+Welcome to Python Blind Auction!
+Enter you name: 
+```
+
+## This project is licensed under the Apache License, Version 2.0. 
+### You're open to make changes and contribute to the project, please make a pull request!
---
+++ main.py
@@ -0,0 +1,63 @@
+# Created by Agamdeep Singh / CodeWithAgam
+# Youtube: CodeWithAgam
+# Github: CodeWithAgam
+# Instagram: @agamdeep_21, @coderagam001
+# Twitter: @CoderAgam001
+# Linkdin: Agamdeep Singh
+
+# Required modules and files
+from os import system, name
+from art import logo
+
+def welcome():
+    # Print the logo
+    print(logo)
+
+    # Print the welcome message
+    print("Welcome to Python Blind Auction!")
+
+def main():
+    # Function to clear the screen
+    def clear():
+        if name == 'nt':
+            _ = system('cls')
+
+        else:
+            _ = system('clear')
+
+    # Function to find the highest bid
+    def find_highest(bids):
+        highest = 0
+        winner = ""
+
+        for bidder in bids:
+            bid_amount = bids[bidder]
+
+            if bid_amount > highest:
+                highest = bid_amount
+                winner = bidder
+        
+        print(f"\nThe winner is: {winner}, with the highest bid as: {highest}")
+
+    # Create an empty dictinoary and set the end to False
+    bids = {}
+    end = False
+
+    # While loop to loop through until the end
+    while not end:
+        # Ask for the inputs
+        username = input("Enter you name: ")
+        bid = int(input("Eneter your bid: "))
+        others = input("\nIs there any other bidder? Type ('yes' or 'no'): ")
+
+        bids[username] = bid
+
+        if others == "no":
+            end = True
+            find_highest(bids)
+
+        else:
+            clear()
+
+welcome()
+main()
